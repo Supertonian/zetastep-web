f7700166f771e105b35af11f476f6f2ebcfedd00
이창윤 <brownbearpower@gmail.com>
2022-10-04 01:47:06 +0900

    fix: add eslint, build error

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 
-const GOOGLE_ANALYTICS_4_ID = '';
 const NAVER_SITE_VERIFICATION = '';
 
 class MyDocument extends Document {
@@ -13,30 +12,9 @@ class MyDocument extends Document {
     return (
       <Html lang="ko">
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {process.env.NODE_ENV === 'production' && (
-            <>
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GOOGLE_ANALYTICS_4_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-                }}
-              />
-              <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
-              <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-              <meta name="naver-site-verification" content={NAVER_SITE_VERIFICATION} />
-            </>
-          )}
+          <meta name="naver-site-verification" content={NAVER_SITE_VERIFICATION} />
+          <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
